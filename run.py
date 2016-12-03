0bc7b9ecf6a1a13828de4a5d75e56b71f42720d9
mpoiitis <mpoiitis@users.noreply.github.com>
2016-12-03 18:48:32 +0200

    Add my life

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import logging
-
+import twitchapi
 from random import randint
 
 from flask import Flask, render_template
@@ -13,45 +13,20 @@ ask = Ask(app, "/")
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+@ask.intent('IsStreaming', mapping={'name': 'Streamer'})
+def IsStreaming(name):
+    if(twitchapi.is_online(name)):
+        return statement(render_template("nai", Streamer = name))
+    else:
+        return statement(render_template("psofa", Streamer = name))
+
 
 @ask.launch
-
 def new_game():
 
     welcome_msg = render_template('welcome')
 
     return question(welcome_msg)
-
-
-@ask.intent("YesIntent")
-
-def next_round():
-
-    numbers = [randint(0, 9) for _ in range(3)]
-
-    round_msg = render_template('round', numbers=numbers)
-
-    session.attributes['numbers'] = numbers[::-1]  # reverse
-
-    return question(round_msg)
-
-
-@ask.intent("AnswerIntent", convert={'first': int, 'second': int, 'third': int})
-
-def answer(first, second, third):
-
-    winning_numbers = session.attributes['numbers']
-
-    if [first, second, third] == winning_numbers:
-
-        msg = render_template('win')
-
-    else:
-
-        msg = render_template('lose')
-
-    return statement(msg)
-
 
 if __name__ == '__main__':
 
